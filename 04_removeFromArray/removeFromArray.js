0ad536b9c20189bfa03123dba4c305e01c82f9a0
Divyu <divyendureddy@gmail.com>
2024-08-13 19:57:40 +0530

    Write the function to remove instances of elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, element) {
+    const newArray = [];
+    for (const i of originalArray) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (j === arguments.length - 1){
+                if (i !== arguments[j]) {
+                    newArray.push(i);;
+                }
+            }
+            else if (i === arguments[j]) {
+                break;
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
