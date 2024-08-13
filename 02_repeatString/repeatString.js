b248b5a12e4f86b4189f34439c5315d3de83d4ea
Divyu <divyendureddy@gmail.com>
2024-08-13 19:31:39 +0530

    Write the function to make it repeat a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
+    let finalText = "";
+    for (let i = 0; i < n; i++) {
+        finalText += string;
+    }
+    return finalText;
 };
 
 // Do not edit below this line
